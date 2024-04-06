183a849741b9074eefa3be5deb935e9b1dca94f7
Abraão Albuquerque <abraao.hs2@gmail.com>
2024-04-06 16:11:32 -0300

    Resolving exercise 05
    Understanding lambda function and implement in to diferent ways.
    
    The first is only a copy of the doubleFunction to a lambda function and
    the second i pass numTimes variable for a global escope to lambda function to multiply numTimes a number.

--- exercise05/main.cpp
+++ exercise05/main.cpp
@@ -11,20 +11,20 @@ int main(int argc, char *argv[])
 
     std::cout << "The double of " << input << " is " << output << std::endl;
 
-
     std::vector<double> myvec{2.3, -6.5, 7.4, 0.996};
 
     // Output vector where we will store the double of the elements in myvec
     std::vector<double> myvec2;
     myvec2.reserve(myvec.size());
-
-
     // TODO Create a lambda function here
 
-
+    auto doubleInput = [](double num) {
+        return num * 2;
+    };
+    
     // Use use a "for each" for iterating the vector -> Note that if the vector
     // was of objects we should use "auto& elem" instead to avoid a copy
-    for(auto elem : myvec) {
+    for(auto &elem : myvec) {
         // TODO: Change doubleInput to your lambda function
         myvec2.push_back(doubleInput(elem));
     }
@@ -35,14 +35,16 @@ int main(int argc, char *argv[])
     }
     std::cout << "]" << std::endl;
 
-
     // Now create another lambda function that receives a double number, but now
     // it captures the variable "numTimes" below and multiply the input by
     // numTimes
     double numTimes = 3.5;
 
-    // TODO Create the lambda function "f2" below
     // auto f2 = [...   // Continue the implementation
+    // this code below is the lambda function that will multiply the input by numTimes the variable numTimes is captured to the fn when i put her in the [] brackets
+    auto f2 = [numTimes](double num) {
+        return num * numTimes;
+    };
 
     // This code will create a std::vector of double numbers and then use the
     // std::transform algorithm from the standard library to call your lambda
@@ -55,5 +57,11 @@ int main(int argc, char *argv[])
     std::cout << "otherVec: ";
     std::copy(otherVec.begin(), otherVec.end(), std::ostream_iterator<double>(std::cout, " "));
 
+    //This code will clear the vectors
+    otherVec.clear();
+    myvec2.clear();
+
+    //This code will hold the window for 'n' seconds
+    sleepSeconds(10);
     return 0;
 }
