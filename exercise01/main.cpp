cd3e569f1ef694a954da47bf50bd2219768ebf4d
Abraão Albuquerque <abraao.hs2@gmail.com>
2024-04-06 01:55:47 -0300

    :sparkles: feat: configure CMakeList, adding sleep on main function to see the result on executable and running project

--- exercise01/main.cpp
+++ exercise01/main.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
 
+#include <thread>
 
 int main(int argc, char *argv[])
 {
     std::cout << "Hello Brave New C++ World" << std::endl;
+    
+    std::this_thread::sleep_for(std::chrono::seconds(2000));
+    // Sleep for 2 seconds to see the output
+    
     return 0;
 }
