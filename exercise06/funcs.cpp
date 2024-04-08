98ec0a4c202c0f1f75f0a53e712d1165808365f3
Abraão Albuquerque <abraao.hs2@gmail.com>
2024-04-08 09:08:21 -0300

    Resolving exercise 06
    Transform exercise 05 resolution with std::vector variables to
    arma::vec. This change will make the some job in only 3 lines because
    the arma::vec can do multiply operator 'without' using a loop in the
    main code.

--- exercise06/funcs.cpp
+++ exercise06/funcs.cpp
@@ -1,6 +1,24 @@
 #include "funcs.h"
-
+#include <iostream>
+#include <thread>
+#include <string>
 
 double doubleInput(double x) {
     return 2 * x;
 }
+
+void sleepSeconds(int seconds) {
+    std::cout <<"\n--------------------------------\n";
+    for (int i = seconds; i != -1; i--)
+    {   
+        std::string str = i>1? "seconds\n":"second\n";
+        if(i==0){
+            std::cout << "\nBye Bye";
+        }else{
+            std::cout  << "This window will close after " << i <<" "<< str;
+        }
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+        
+    }
+}
+
--- exercise06/funcs.h
+++ exercise06/funcs.h
@@ -4,5 +4,6 @@
 
 // Add the function prototype here for the "doubleInput" function
 double doubleInput(double x);
+void sleepSeconds(int seconds);
 
 #endif
